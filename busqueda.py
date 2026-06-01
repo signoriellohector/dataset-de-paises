ed22a157a4e3539f8586f4a2ee9b6dcec0c22c14
Signoriello Hector <hector.signoriello@gmail.com>
2026-05-31 21:07:26 -0300

    Creacion de utilidades.py e implementacion busqueda_por_nombre_parcial

--- busqueda.py
+++ busqueda.py
@@ -8,9 +8,9 @@
 # Grupo 72
 #
 # Modulo de búsqueda
+import manejo_dataset
 
-
-def busqueda_por_nombre(paises:list,nombre:str):
+def busqueda_por_nombre(paises:list,nombre:str)->list:
     # creación de lista auxiliar
     coincidencias = [] 
 
@@ -26,3 +26,25 @@ def busqueda_por_nombre(paises:list,nombre:str):
 
     return coincidencias
 
+
+def busqueda_por_nombre_parcial(paises:list, nombre:str)->list:
+        # creación de lista auxiliar
+    coincidencias = [] 
+
+    # Normalizando string
+    # strip() elimina espacios en blanco antes y después de la cadena de caracteres
+    # lower() convierte todos los caracteres en minúsculas
+    nombre= nombre.strip().lower()
+    
+    # recorrido de lista hasta encontrar todas las coincidencias posibles
+    for pais in paises:
+        nombre_pais = pais['nombre'].strip().lower()
+        if nombre_pais.startswith(nombre):
+            coincidencias.append(pais)
+
+    return coincidencias
+
+
+if __name__=="__main__":
+    paises = manejo_dataset.cargar_paises()
+    print(busqueda_por_nombre_parcial(paises,'a'))
